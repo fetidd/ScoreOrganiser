0155e7989cf227c4290b3afcf0ed53d97fc49615
Ben Jones <ben@reticent-monolith.com>
2022-12-01 14:13:36 +0000

    dfg

--- scorg/src/StudentList/StudentList.tsx
+++ scorg/src/StudentList/StudentList.tsx
@@ -1,7 +1,18 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import AddStudentDialog from "./dialogs/AddStudent";
+import DeleteStudentDialog from "./dialogs/DeleteStudent";
+import EditStudentDialog from "./dialogs/EditStudent";
 import { Student } from "./Student"
 import StudentRow from "./StudentRow";
+
+
+interface Props {
+    students: Student[],
+    setStudents: Function,
+    selected: string,
+    select: Function,
+    getStudents: Function
+}
 
 export default function StudentList({ students, setStudents, selected, select, getStudents }: Props) {
     const [idCount, setIdCount] = useState(5) // TODO remove, just for dev
@@ -20,20 +31,20 @@ export default function StudentList({ students, setStudents, selected, select, g
     const [deleteConfirmationTarget, setDeleteConfirmationTarget] = useState("")
     const [file, setFile] = useState(null as File | null)
 
-    const addStudent = () => { // TODOINVOKE
+    const _addStudent = () => { // TODOINVOKE
         let newStudent: Student = { id: `st${idCount}`, name: addName, dob: addDob }
         setIdCount(idCount+1)
         students.push(newStudent)
         setStudents(students)
     }
 
-    const deleteStudent = () => { // TODOINVOKE
+    const _deleteStudent = () => { // TODOINVOKE
         let newStudents = students.filter(st => st.id !== deleting)
         setStudents(newStudents)
         setDeleting("")
     }
 
-    const editStudent = () => { // TODOINVOKE
+    const _editStudent = () => { // TODOINVOKE
         let edited = students.find(st => st.id === editing)
         if (edited !== null && edited !== undefined) {
             edited.name = editName
@@ -42,45 +53,48 @@ export default function StudentList({ students, setStudents, selected, select, g
         setEditing("")
     }
 
-    useEffect(() => { getStudents() }, [])
-
+    
     const closeModals = () => {
         setShowAddStudent(false)
         setShowEditStudent(false)
         setShowDeleteStudent(false)
         setModal(false)
     }
-
+    
     const uploadFile = () => {
         console.log(file);
         (document.querySelector("#csv-input") as HTMLInputElement).value = ""
         setFile(null)
     }
+    
+    useEffect(() => { getStudents() }, [])
+
+    function handleAddStudent() {
+        setModal(true)
+        setShowAddStudent(true)
+    }
+
+    function handleFileChange(e: React.FormEvent) {
+        const files = (e.target as HTMLInputElement).files
+        if (files !== null && files[0] !== null) {
+            setFile(files[0])
+        }
+    }
 
     const rows = students.map(student => {
         return (
-            <StudentRow 
-                key={student.id}
+            <StudentRow key={student.id}
                 student={student}
                 select={select}
                 selected={selected}
-                showEditStudent={showEditStudent}
                 setShowEditStudent={setShowEditStudent}
-                editName={editName}
-                editDob={editDob}
                 setEditName={setEditName}
                 setEditing={setEditing}
                 setEditDob={setEditDob}
-                editStudent={editStudent}
-                showDeleteStudent={showDeleteStudent}
                 setShowDeleteStudent={setShowDeleteStudent}
                 setDeleting={setDeleting}
-                confirmDelete={confirmDelete}
                 setConfirmDelete={setConfirmDelete}
-                deleteConfirmationTarget={deleteConfirmationTarget}
                 setDeleteConfirmationTarget={setDeleteConfirmationTarget}
-                deleteStudent={deleteStudent}
-                closeModals={closeModals}
                 setModal={setModal}
             />
         )
@@ -90,67 +104,26 @@ export default function StudentList({ students, setStudents, selected, select, g
         <>
             <div id="StudentList">
                 <div id="menubar-area">
-                    <button
-                        className="icon-button dark"
-                        onClick={() => {
-                            setModal(true)
-                            setShowAddStudent(true)
-                        }}><i className="fa-solid fa-plus"></i></button>
+                    <button className="icon-button dark"onClick={() => {handleAddStudent()}}>
+                        <i className="fa-solid fa-plus"></i>
+                    </button>
                 </div>
-
                 <div id="list-area">
                     <ul id="student-list">
                         {rows}
                     </ul>
                 </div>
-
                 <div id="import-csv-area">
                     <div className="row">
-                        <input
-                            id="csv-input"
-                            type="file"
-                            name="csv-input"
-                            accept=".csv"
-                            onChange={e => {
-                                const files = e.target.files
-                                if (files !== null && files[0] !== null) {
-                                    setFile(files[0])
-                                }
-                            }}
-                        />
-                        <button
-                            className="button"
-                            onClick={uploadFile}
-                        >Upload</button>
+                        <input id="csv-input" type="file" name="csv-input" accept=".csv" onChange={e => {handleFileChange(e)}}/>
+                        <button className="button" onClick={uploadFile}>Upload</button>
                     </div>
                 </div>
             </div>
-
-            <div
-                className="modal"
-                onClick={() => closeModals()}
-                style={{
-                    display: modal ? "block" : "none",
-                }}></div>
-
-            <AddStudentDialog
-                showAddStudent={showAddStudent}
-                addName={addName}
-                setAddName={setAddName}
-                addDob={addDob}
-                setAddDob={setAddDob}
-                addStudent={addStudent}
-                closeModals={closeModals}
-            />
+            <div className="modal" onClick={() => closeModals()} style={{display: modal ? "block" : "none",}}></div>
+            <AddStudentDialog showAddStudent={showAddStudent} addName={addName} setAddName={setAddName} addDob={addDob} setAddDob={setAddDob} addStudent={_addStudent} closeModals={closeModals} />
+            <EditStudentDialog showEditStudent={showEditStudent} editName={editName} editDob={editDob} setEditName={setEditName} setEditDob={setEditDob} editStudent={_editStudent} closeModals={closeModals} />
+            <DeleteStudentDialog showDeleteStudent={showDeleteStudent} confirmDelete={confirmDelete} setConfirmDelete={setConfirmDelete} deleteConfirmationTarget={deleteConfirmationTarget} deleteStudent={_deleteStudent} closeModals={closeModals} setDeleteConfirmationTarget={setDeleteConfirmationTarget} />
         </>
     )
 }
-
-type Props = {
-    students: Student[],
-    setStudents: Function,
-    selected: string,
-    select: Function,
-    getStudents: Function
-}
-
--- scorg/src/StudentList/StudentRow.tsx
+++ scorg/src/StudentList/StudentRow.tsx
@@ -1,30 +1,33 @@
-import { useState } from "react";
-import DeleteStudentDialog from "./dialogs/DeleteStudent";
-import EditStudentDialog from "./dialogs/EditStudent";
+import React, { useState } from "react";
 import { Student } from "./Student";
 
-export default function StudentRow ({
+
+type Props = {
+    student: Student,
+    select: Function,
+    selected: string,
+    setShowEditStudent: Function,
+    setEditName: Function,
+    setEditing: Function,
+    setEditDob: Function,
+    setShowDeleteStudent: Function,
+    setDeleting: Function,
+    setConfirmDelete: Function,
+    setDeleteConfirmationTarget: Function,
+    setModal: Function,
+}
+
+export default function StudentRow({
     student,
     select,
     selected,
-
-    showEditStudent,
     setShowEditStudent,
-    editName,
-    editDob,
     setEditName,
     setEditing,
     setEditDob,
-    editStudent,
-    showDeleteStudent,
     setShowDeleteStudent,
     setDeleting,
-    confirmDelete,
-    setConfirmDelete,
-    deleteConfirmationTarget,
     setDeleteConfirmationTarget,
-    deleteStudent,
-    closeModals,
     setModal,
 }: Props) {
 
@@ -33,102 +36,42 @@ export default function StudentRow ({
     const isSelected = student.id === selected;
     let classes = "student-row"
     if (isSelected) classes += " selected"
+
+    function handleEditClick(e: React.MouseEvent) {
+        e.stopPropagation()
+        setModal(true)
+        setEditing(student.id)
+        setEditName(student.name)
+        setEditDob(student.dob)
+        setShowEditStudent(true)
+    }
+
+    function handleDeleteClick(e: React.MouseEvent) {
+        e.stopPropagation()
+        setModal(true)
+        setDeleting(student.id)
+        const splitName = student.name.split(" ")
+        setDeleteConfirmationTarget(splitName[splitName.length - 1])
+        setShowDeleteStudent(true)
+    }
+
+    function handleRightClick(e: React.MouseEvent) {
+        e.preventDefault()
+        select(student.id)
+        setHasContextFocus(true)
+    }
+
     return (
         <li>
-            <div
-                className={classes}
-                onClick={() => {
-                    select(student.id)
-                }}
-                onContextMenu={e => {
-                    e.preventDefault()
-                    select(student.id)
-                    setHasContextFocus(true)
-                }}
-                onMouseLeave={() => {
-                    setHasContextFocus(false)
-                }}
-            >
+            <div className={classes} onClick={() => {select(student.id)}} onContextMenu={e => {handleRightClick(e)}} onMouseLeave={() => {setHasContextFocus(false)}} >
                 <span>{student.name}</span>
-                <button
-                    className="icon-button"
-                    style={{
-                        display: (hasContextFocus) ? "block" : "none",
-                        justifySelf: "end"
-                    }}
-                    onClick={e => {
-                        e.stopPropagation()
-                        setEditName(student.name)
-                        setEditDob(student.dob)
-                        setModal(true)
-                        setEditing(student.id)
-                        setShowEditStudent(true)
-                    }}
-                ><i className="fa-solid fa-pen"></i></button>
-                <button
-                    className="icon-button red"
-                    style={{
-                        display: (hasContextFocus) ? "block" : "none",
-                        justifySelf: "end"
-                    }}
-                    onClick={e => {
-                        e.stopPropagation()
-                        setShowDeleteStudent(true)
-                        setModal(true)
-                        setDeleting(student.id)
-                        const splitName = student.name.split(" ")
-                        setDeleteConfirmationTarget(splitName[splitName.length - 1])
-                    }}
-                ><i className="fa-solid fa-trash-can"></i></button>
+                <button className="icon-button" style={{display: (hasContextFocus) ? "block" : "none", justifySelf: "end"}} onClick={e => {handleEditClick(e)}}>
+                    <i className="fa-solid fa-pen"></i>
+                </button>
+                <button className="icon-button red" style={{display: (hasContextFocus) ? "block" : "none", justifySelf: "end"}} onClick={e => {handleDeleteClick(e)}} >
+                    <i className="fa-solid fa-trash-can"></i>
+                </button>
             </div>
-
-            <EditStudentDialog
-                showEditStudent={showEditStudent}
-                editName={editName}
-                editDob={editDob}
-                setEditName={setEditName}
-                setEditDob={setEditDob}
-                editStudent={editStudent}
-                closeModals={closeModals}
-            />
-
-            <DeleteStudentDialog
-                showDeleteStudent={showDeleteStudent}
-                confirmDelete={confirmDelete}
-                setConfirmDelete={setConfirmDelete}
-                deleteConfirmationTarget={deleteConfirmationTarget}
-                deleteStudent={deleteStudent}
-                closeModals={closeModals}
-                setDeleteConfirmationTarget={setDeleteConfirmationTarget}
-            />
-
         </li>
     )
-}
-
-type Props = {
-    student: Student,
-    select: Function,
-    selected: string,
-
-    showEditStudent: boolean,
-    setShowEditStudent: Function,
-    editName: string, 
-    editDob: string,
-    setEditName: Function,
-    setEditing: Function,
-    setEditDob: Function, 
-    editStudent: Function,
-
-    showDeleteStudent: boolean,
-    setShowDeleteStudent: Function,
-    setDeleting: Function,
-    confirmDelete: string,
-    setConfirmDelete: Function,
-    deleteConfirmationTarget: string,
-    setDeleteConfirmationTarget: Function,
-    deleteStudent: Function,
-
-    closeModals: Function,
-    setModal: Function,
 }
